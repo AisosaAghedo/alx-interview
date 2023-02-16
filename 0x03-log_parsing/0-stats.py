c4d8552f8fef9d229e253daeedb5f0de0b0e6ff2
Aisosa Aghedo <allysonaghedo@gmail.com>
2023-02-16 22:17:43 +0000

    Log parsing task

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""alx interview algorithm question on log parsing, 
+"""alx interview algorithm question on log parsing,
 a script that reads stdin line by line and computes metrics"""
+
+
 import sys
 
 cache = {'200': 0, '301': 0, '400': 0, '401': 0,
