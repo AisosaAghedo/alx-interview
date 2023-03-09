44973b288a05c85e5c5090f13d2fcb6787d2335c
Aisosa Aghedo <allysonaghedo@gmail.com>
2023-03-09 21:15:37 +0000

    Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,24 +4,24 @@ const req = require('request');
 const id = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/';
 
-function new_promise(link) {
-return new Promise((resolve)=>{
-req.get(link, function (error, res, body) {
-  if (error) {
-    console.log(error);
-  }
-  const data = JSON.parse(body);
-  resolve(data);
-});
-});
+function newPromise (link) {
+  return new Promise((resolve) => {
+    req.get(link, function (error, res, body) {
+      if (error) {
+        console.log(error);
+      }
+      const data = JSON.parse(body);
+      resolve(data);
+    });
+  });
 }
 
-async function prom(){
-	const characters = await  new_promise(url + id)
+async function prom () {
+  const characters = await newPromise(url + id);
 
-	for (let character of characters.characters){
-		let name = await new_promise(character)
-		console.log(name.name);
-	}
+  for (const character of characters.characters) {
+    const name = await newPromise(character);
+    console.log(name.name);
+  }
 }
 prom();
